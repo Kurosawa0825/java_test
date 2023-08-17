37061297f53b3ea040c0bd747b08b45c62704363
Kurosawa0825 <kusunoki19980825@gmail.com>
2023-08-17 12:09:17 +0900

    javascript_学習④

---
+++ lesson4/script1.js
@@ -0,0 +1,10 @@
+const animal = {
+  name: "レオ",
+  age: 3,
+  greet: () => {
+    console.log("こんにちは");
+  },
+};
+console.log(animal.name);
+
+animal.greet();
---
+++ lesson4/script7.js
@@ -0,0 +1,19 @@
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log("こんにちは");
+  }
+
+  info() {
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+const animal = new Animal("レオ", 3);
+animal.greet();
+
+animal.info();
