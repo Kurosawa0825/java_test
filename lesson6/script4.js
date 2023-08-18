fb86dea92908aea5a7b49bf7a02a9d866f05e066
Kurosawa0825 <kusunoki19980825@gmail.com>
2023-08-18 10:55:40 +0900

    javascript_学習⑥

---
+++ lesson6/script4.js
@@ -0,0 +1,19 @@
+const numbers = [1, 2, 3, 4];
+
+const evenNumbers = numbers.filter((number) => {
+  return number % 2 === 0;
+});
+
+console.log(evenNumbers);
+
+const characters = [
+  { id: 1, name: "にんじゃわんこ", age: 14 },
+  { id: 2, name: "ベイビーわんこ", age: 5 },
+  { id: 3, name: "ひつじ仙人", age: 100 },
+];
+
+const underTwenty = characters.filter((character) => {
+  return character.age < 20;
+});
+
+console.log(underTwenty);
